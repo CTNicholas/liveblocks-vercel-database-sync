165067a1323c9d5bdbd30f08f72ff079f4ca6c3d
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-05-01 17:58:45 +0100

    feat: Test endpoint

---
+++ src/routes/api/webhook/+server.ts
@@ -0,0 +1,20 @@
+//import { sql } from "@vercel/postgres";
+//import { redirect, type RequestEvent } from "@sveltejs/kit";
+
+export async function POST(/*{ request } : RequestEvent*/) {
+  console.log("webhook");
+  /*
+  const data = await request.formData();
+  const { rows } = await sql`
+      INSERT INTO products (name)
+      VALUES (${data.get('name')})
+    `;
+
+  throw redirect(303, `/product/${rows[0].slug}`);
+}
+
+   */
+
+  return new Response(JSON.stringify({ success: true }), { status: 400 });
+  //return new Response(response.body, { status: response.status });
+}
