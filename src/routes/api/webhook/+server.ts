04627c19bbec5293a3d2150bc59e47069dcbf323
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2023-05-01 21:22:02 +0100

    feat: Create if not exists

--- src/routes/api/webhook/+server.ts
+++ src/routes/api/webhook/+server.ts
@@ -1,19 +1,18 @@
 import { sql } from "@vercel/postgres";
-//import { redirect, type RequestEvent } from "@sveltejs/kit";
-
-import type { RequestEvent } from "@sveltejs/kit";
+import { error, type RequestEvent } from "@sveltejs/kit";
 
 const API_KEY = import.meta.env.VITE_LIVEBLOCKS_SECRET_KEY as string;
 
+// This is called by a Liveblocks "StorageUpdatedEvent" webhook
+// https://liveblocks.io/docs/guides/webhooks#StorageUpdatedEvent
 export async function POST({ request }: RequestEvent) {
-  console.log("webhook request:");
-
   const { data } = await request.json();
 
   if (!data) {
-    throw new Error("Cannot get data from webhook");
+    throw error(400, "Cannot get data from webhook");
   }
 
+  // Get storage contents
   const response = await fetch(
     `https://api.liveblocks.io/v2/rooms/${data.roomId}/storage`,
     {
@@ -24,64 +23,39 @@ export async function POST({ request }: RequestEvent) {
   );
 
   if (!response.ok) {
-    throw new Error("Error getting storage");
+    throw error(400, "Error getting storage");
   }
 
   const storage = await response.json();
 
   if (!storage?.data?.article?.data) {
-    throw new Error("No article data");
+    throw error(400, "No article data");
   }
 
   const { title, content, date, publish } = storage.data.article.data;
 
-  console.log(data, storage.data.article.data);
-
-  const sqlResponse =
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS articles (
+  id VARCHAR(255) PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  content TEXT NOT NULL,
+  date INTEGER NOT NULL,
+  publish BOOLEAN NOT NULL
+);`;
     await sql`INSERT INTO articles (id, title, content, date, publish)
-VALUES (${data.roomId}, ${title}, ${content}, ${date}, ${publish})
-ON CONFLICT (id)
-DO UPDATE SET
-  title = EXCLUDED.title,
-  content = EXCLUDED.content,
-  date = EXCLUDED.date,
-  publish = EXCLUDED.publish;
-`;
-
-  console.log(sqlResponse);
-
-  /*
-  const data = await request.formData();
-  const { rows } = await sql`
-      INSERT INTO products (name)
-      VALUES (CHRIS)
-    `;
-    */
+  VALUES (${data.roomId}, ${title}, ${content}, ${date}, ${publish})
+  ON CONFLICT (id)
+  DO UPDATE SET
+    title = EXCLUDED.title,
+    content = EXCLUDED.content,
+    date = EXCLUDED.date,
+    publish = EXCLUDED.publish;
+  `;
+  } catch (err) {
+    throw error(400, "No article data");
+  }
 
   return new Response(JSON.stringify({ success: true, roomId: data.roomId }), {
     status: 200,
   });
 }
-
-/*
-export async function POST({ request } : RequestEvent) {
-  console.log("webhook");
-
-  console.log(request.body);
-
-
-  const data = await request.formData();
-  const { rows } = await sql`
-      INSERT INTO products (name)
-      VALUES (CHRIS)
-    `;
-
-  throw redirect(303, `/product/${rows[0].slug}`);
-}
-
-
-
-return new Response(JSON.stringify({ success: true }), { status: 400 });
-//return new Response(response.body, { status: response.status });
-}
- */
